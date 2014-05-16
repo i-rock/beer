1663e77d9d55ab833620ffdb65c14eedb7f5d0c2
i-rock <thisclap@gmail.com>
2014-05-16 17:19:10 -0400

    lookin good

--- beerSearch.php
+++ beerSearch.php
@@ -1,17 +1,18 @@
 <?php 
 require_once("plugins/Brewerydb.php");
-if(isset($_GET)) {
+if(isset($_GET['query']) && isset($_GET['page'])) {
 
 	$bdb = new Pintlabs_Service_Brewerydb('0128d8396729aa2d96a304583194c471');
 	$bdb->setFormat('json');
 
 	try {
-		$params = array('p'=>'1', 'q'=>$_GET['query'], 'type'=>'beer', 'withBreweries'=>'Y');
+		$params = array('p'=>$_GET['page'], 'q'=>$_GET['query'], 'type'=>'beer', 'withBreweries'=>'Y');
 		$results = $bdb->request('search', $params, 'GET');
 	} catch (Exception $e) {
 		$results = array('error' => $e->getMessage());
 	}
 
+	//TEST CODE
 	/*try {
 		$params = array('abv' => '10.5', 'p' => '1');
 		$results = $bdb->request('beers', $params, 'GET');
@@ -19,9 +20,7 @@ if(isset($_GET)) {
 		$results = array('error' => $e->getMessage());
 	}*/
 
-	echo json_encode($results);
-
-/*	print_r($results['data'][0]);
+	/*print_r($results['data'][0]);
 
 	print_r('number of pages: '.$results['numberOfPages']);
 	echo '<br/><br/>';
@@ -30,6 +29,8 @@ if(isset($_GET)) {
 		print_r($res['breweries'][0]['name'].' '.$res['name']);
 		echo '<br/>';
 	}*/
+
+	echo json_encode($results); // return result
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,17 @@
+<h1>hello world</h1>
+
+<form id='searchForm'>
+	<input type='text' id='searchInput' />
+	<input type='submit' value='Submit' />
+</form>
+
+<div id='content'>
+	<div id='beerPic'></div>
+
+	<div id='resultsDisplay'></div>
+</div>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
 <script>
 var json = null;
 
@@ -7,50 +20,103 @@ function showBeer(i) {
 		console.log('json is null');
 		return false;
 	}
-	var name = ""+json['data'][i]['breweries'][0]['name']+' '+json['data'][i]['name'], image='';
 
-	if(json['data'][i]['breweries'][0]['images'] !== undefined) {
-		image = "<img src='"+json['data'][i]['breweries'][0]['images']['large']+"' />";
+	var name = ""+json.data[i].breweries[0].name+' '+json.data[i].name, image='';
+
+	if(json.data[i].breweries[0].images !== undefined) {
+		image = "<img src='"+json.data[i].breweries[0].images.large+"' />";
 		$('#beerPic').html(image);
 	} else {
-		$('#beerPic').html('')
+		$('#beerPic').html('');
 	}
 
 	return false;
 }
 
 $(function() {
-	$('#searchInput').focus();
-	$('#searchForm').submit(function (event) {
-		var input = $('#searchInput').val();
-		event.preventDefault();
+	var currPage = 0;
+	var noPages = 0;
+	var query = "";
+	function getBeer(input, page) {
 		if(input.length > 0) {
+			query = input;
 			$.ajax({
 				type: "GET",
 				dataType: "json",
 				url: "beerSearch.php",
-				data: { query: input },
+				data: { query: input, page: page },
 				success: function (data) {
-					json = data;
-					$.each(data['data'], function(i, item) {
-						$('#resultsDisplay').append("<a href='javascript:void(0)' onclick='showBeer("+i+")'>"+data['data'][i]['breweries'][0]['name']+' '+data['data'][i]['name']+'</a><br/>');
-					});				
+					if (!data) { // check that data was returned
+						console.log('no data returned');
+						return false;
+					}
+					var beerObj = {};
+					//var breweryArray = [];
+					json = data; // move it global
+
+					//clear content children
+					//$('#content div').html('');
+					$('#resultsDisplay').html('');
+					$('#beerPic').html('');
+
+					//Get unique breweries
+					/*for(var i=0; i<data.data.length; i++) {
+						var breweryName = data.data[i].breweries[0].name;
+						if(breweryName.length > 0 && breweryArray.indexOf(breweryName) === -1) {
+							breweryArray.push(breweryName);
+						}
+						
+					}*/
+
+					//Add beers to appropriate breweries
+					for(var i=0; i<data.data.length; i++) {
+						if(beerObj[data.data[i].breweries[0].name] === undefined) {
+							beerObj[data.data[i].breweries[0].name] = [data.data[i].name];
+						} else {
+							beerObj[data.data[i].breweries[0].name].push(data.data[i].name);
+						}
+					}
+
+					for(var brewery in beerObj) {
+						$('#resultsDisplay').append("<h3>"+brewery+"</h3>");
+						$('#resultsDisplay').append("<ul>");
+						for(var i=0; i<beerObj[brewery].length; i++) {
+							$('#resultsDisplay').append("<li style='margin-left: 1em;'>"+beerObj[brewery][i]+"</li>");
+						}
+						$('#resultsDisplay').append("</ul>");
+					}
+
+					currPage = data.currentPage;
+					noPages = data.numberOfPages;
+
+					if(currPage != noPages) {	
+						$('#resultsDisplay').append("Page "+currPage+"/"+noPages+"&nbsp;&nbsp;<a href='javascript:void(0)' id='moreClick'>more..</a>");
+						currPage++;
+					}
+
 				}
 			});
 		}
-		$('#searchInput').val('');
+		$('#searchInput').val('');		
+	}
+
+	$(document).on('click', '#moreClick', function (e) {
+		getBeer(query, currPage);
+		e.preventDefault();
 	});
+
+	$('#searchForm').submit(function (event) {
+		event.preventDefault();
+		var input = $('#searchInput').val();
+		getBeer(input, 1);
+	});
+
+	$('#searchInput').focus();
+
 })
+
+
 </script>
 
-<h1>hello world</h1>
 
-<form id='searchForm'>
-	<input type='text' id='searchInput' />
-	<input type='submit' value='Submit' />
-</form>
 
-<div id='beerPic'></div>
-
-<div id='resultsDisplay'>
-</div>
