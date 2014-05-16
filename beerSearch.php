af0fb1839497a99e6341c9a91c0c041e653855bd
ian <ian@ian-vb.(none)>
2014-05-15 22:49:55 -0400

    moved data handling over to client

--- beerSearch.php
+++ beerSearch.php
@@ -1,29 +1,35 @@
 <?php 
+require_once("plugins/Brewerydb.php");
+if(isset($_GET)) {
 
-require("plugins/Brewerydb.php");
+	$bdb = new Pintlabs_Service_Brewerydb('0128d8396729aa2d96a304583194c471');
+	$bdb->setFormat('json');
 
-$bdb = new Pintlabs_Service_Brewerydb('0128d8396729aa2d96a304583194c471');
-$bdb->setFormat('php');
+	try {
+		$params = array('p'=>'1', 'q'=>$_GET['query'], 'type'=>'beer', 'withBreweries'=>'Y');
+		$results = $bdb->request('search', $params, 'GET');
+	} catch (Exception $e) {
+		$results = array('error' => $e->getMessage());
+	}
 
-try {
-	$params = array('p'=>'1', 'q'=>'yuengling', 'type'=>'beer');
-	$results = $bdb->request('search', $params, 'GET');
-} catch (Exception $e) {
-	$results = array('error' => $e->getMessage());
+	/*try {
+		$params = array('abv' => '10.5', 'p' => '1');
+		$results = $bdb->request('beers', $params, 'GET');
+	} catch (Exception $e) {
+		$results = array('error' => $e->getMessage());
+	}*/
+
+	echo json_encode($results);
+
+/*	print_r($results['data'][0]);
+
+	print_r('number of pages: '.$results['numberOfPages']);
+	echo '<br/><br/>';
+
+	foreach ($results['data'] as $res) {
+		print_r($res['breweries'][0]['name'].' '.$res['name']);
+		echo '<br/>';
+	}*/
 }
 
-/*try {
-	$params = array('abv' => '10.5', 'p' => '1');
-	$results = $bdb->request('beers', $params, 'GET');
-} catch (Exception $e) {
-	$results = array('error' => $e->getMessage());
-}*/
-
-print_r('number of pages: '.$results['numberOfPages']);
-echo '<br/><br/>';
-
-foreach ($results['data'] as $res) {
-	print_r($res['name']);
-	echo '<br/>';
-}
 ?>
--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script>
+
 $(function() {
-	$('#searchForm').submit(function(event) {
+	$('#searchInput').focus();
+	$('#searchForm').submit(function (event) {
 		var input = $('#searchInput').val();
+		event.preventDefault();
 		if(input.length > 0) {
-			
+			$.ajax({
+				type: "GET",
+				dataType: "json",
+				url: "beerSearch.php",
+				data: { query: input },
+				success: function (data) {
+					$.each(data['data'], function(i, item) {
+						$('#resultsDisplay').append(data['data'][i]['breweries'][0]['name']+' '+data['data'][i]['name']+'<br/>');
+					});				
+				}
+			});
 		}
 		$('#searchInput').val('');
-		return false;
 	});
 })
 </script>
@@ -15,7 +27,9 @@ $(function() {
 <h1>hello world</h1>
 
 <form id='searchForm'>
-	<input type='text' name='searchInput' id='searchInput' />
+	<input type='text' id='searchInput' />
 	<input type='submit' value='Submit' />
 </form>
 
+<div id='resultsDisplay'>
+</div>
